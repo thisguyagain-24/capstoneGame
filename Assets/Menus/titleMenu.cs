2ea65c0c0278cd92440f8fcd551be2008b69b4eb
thisguyagain-39 <cframpto@confederationcollege.ca>
2025-09-22 15:31:19 -0400

    menus exist now? they dont work yet though

---
+++ Assets/Menus/titleMenu.cs
@@ -0,0 +1,19 @@
+// script for title menu / start screen, it just listens for any input and then changes scene if it catches smth
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenInput : MonoBehaviour
+{
+    private bool keyPressed = false;
+
+    private void Update()
+    {  
+        if (!keyPressed && Input.anyKeyDown)
+        {
+            keyPressed = true;
+
+            SceneManager.LoadScene("mainMenu");
+        }
+    }
+}
