d0e5128b20ef32d7d429b90d7cefba0e6e688227
thisguyagain-39 <cframpto@confederationcollege.ca>
2025-09-24 12:24:27 -0400

    saaaaaaaa

--- Assets/Menus/mainMenu.cs
+++ Assets/Menus/mainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class mainMenu : MonoBehaviour
@@ -10,5 +11,34 @@ public class mainMenu : MonoBehaviour
     [SerializeField]
     public GameObject[] menubuttons;
 
-    
+    void VersusSelected() 
+    {
+
+        // play sound effect, play animation, then load versus scene
+
+        // hmmmmm sfx and animation can probably be in their own reusable function once im putting those in
+
+    }
+
+    void StorySelected() 
+    {
+
+        // popup that tells you its not ready yet
+
+    }
+
+    void SettingsSelected() 
+    {
+
+        // load settings menu
+
+    }
+
+    void ReturnToTitle() {
+
+        // play sound effect, play animation, then load title menu
+
+        SceneManager.LoadScene("startMenu");
+
+    }
 }
