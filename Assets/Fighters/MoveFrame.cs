bd7b80058675b11caa3abf0cb83ccc989636bd27
Aevo55 <dawsgilmore@gmail.com>
2025-10-10 13:26:46 -0400

    IM COOKING, AGAIN

---
+++ Assets/Fighters/MoveFrame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFrame : MonoBehaviour
+{
+    [Min(1)]
+    [Tooltip("Duration of move frame")]
+    public int duration;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
